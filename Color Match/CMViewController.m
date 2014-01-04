468b913293d344f5b754774ccff3a59db7f9af69
linda167 <linda167@gmail.com>
2014-01-04 11:31:45 -0800

    Color lines

--- Color Match/CMViewController.m
+++ Color Match/CMViewController.m
@@ -16,6 +16,8 @@
 @property NSArray *leftColorButtons;
 @property NSMutableArray *leftColorsState;
 @property NSMutableArray *colorCellSections;
+@property NSMutableArray *verticalLines;
+@property NSMutableArray *horizontalLines;
 @end
 
 @implementation CMViewController
@@ -42,6 +44,7 @@
     self.colorCellSections = [NSMutableArray arrayWithObjects:row1, row2, row3, nil];
     
     [self DrawVerticalConnectingLines];
+    [self DrawHorizontalConnectingLines];
 }
 
 - (void)didReceiveMemoryWarning
@@ -110,41 +113,50 @@
 }
 
 - (IBAction)GridButtonPressed:(id)sender {
-    // Update filled color on the button being clicked
+
     int selectedColor = (int)self.selectedColor;
-    if (selectedColor == 0)
-    {
-        UIImage *btnImage1 = [UIImage imageNamed:@"EmptyCircle.png"];
-        [sender setImage:btnImage1 forState:UIControlStateNormal];
-    }
-    else if (selectedColor == 1)
-    {
-        UIImage *btnImage1 = [UIImage imageNamed:@"FilledCircleBlue.png"];
-        [sender setImage:btnImage1 forState:UIControlStateNormal];
-    }
-    else if (selectedColor == 2)
-    {
-        UIImage *btnImage1 = [UIImage imageNamed:@"FilledCircleRed.png"];
-        [sender setImage:btnImage1 forState:UIControlStateNormal];
-    }
-    else if (selectedColor == 3)
-    {
-        UIImage *btnImage1 = [UIImage imageNamed:@"FilledCircleYellow.png"];
-        [sender setImage:btnImage1 forState:UIControlStateNormal];
-    }
     
     // Update color state on top and left bar
     NSNumber* wrappedSelectedColor = [NSNumber numberWithInt:selectedColor];
     NSInteger topIndex = [self.topColorButtons indexOfObject:sender];
+    UIView *lineToUpdate;
     if (topIndex != NSNotFound)
     {
         [self.topColorsState replaceObjectAtIndex:topIndex withObject:wrappedSelectedColor];
+        lineToUpdate = [self.verticalLines objectAtIndex:topIndex];
     }
     else
     {
         NSInteger leftIndex = [self.leftColorButtons indexOfObject:sender];
         // Should not be NSNotFound at ths point
         [self.leftColorsState replaceObjectAtIndex:leftIndex withObject:wrappedSelectedColor];
+        lineToUpdate = [self.horizontalLines objectAtIndex:leftIndex];
+    }
+    
+    // Update filled color on the button being clicked, and the color of the connecting line
+    if (selectedColor == 0)
+    {
+        UIImage *btnImage1 = [UIImage imageNamed:@"EmptyCircle.png"];
+        [sender setImage:btnImage1 forState:UIControlStateNormal];
+        lineToUpdate.backgroundColor = [self GetGrayColor];
+    }
+    else if (selectedColor == 1)
+    {
+        UIImage *btnImage1 = [UIImage imageNamed:@"FilledCircleBlue.png"];
+        [sender setImage:btnImage1 forState:UIControlStateNormal];
+        lineToUpdate.backgroundColor = [self GetBlueColor];
+    }
+    else if (selectedColor == 2)
+    {
+        UIImage *btnImage1 = [UIImage imageNamed:@"FilledCircleRed.png"];
+        [sender setImage:btnImage1 forState:UIControlStateNormal];
+        lineToUpdate.backgroundColor = [self GetRedColor];
+    }
+    else if (selectedColor == 3)
+    {
+        UIImage *btnImage1 = [UIImage imageNamed:@"FilledCircleYellow.png"];
+        [sender setImage:btnImage1 forState:UIControlStateNormal];
+        lineToUpdate.backgroundColor = [self GetYellowColor];
     }
     
     // Trigger update of color cells
@@ -160,25 +172,73 @@
     
     // We should assert that top connections and bottom connections have equal number of items
     int itemCount = (int)[topConnections count];
+    NSMutableArray *verticalLines = [NSMutableArray array];
     for (int i=0; i<itemCount; i++)
     {
-        // TODO: lindach
-        // Test drawing a line
+        // Draw line
         UIView *topConnection = [topConnections objectAtIndex:i];
         int topAdjustment = -13;   // Accounts for extra spacing in top button
         int topY = topConnection.frame.origin.y + topConnection.frame.size.height + topAdjustment;
         int xAdjustment = -1; // Account for the fact that our width is 3 pixels
         int topX = topConnection.frame.origin.x + topConnection.frame.size.width / 2 + xAdjustment;
         
-        UIImageView *bottomConnection = [bottomConnections objectAtIndex:i];;
+        UIImageView *bottomConnection = [bottomConnections objectAtIndex:i];
         int bottomAdjustment = 3;   // Accounts for extra spacing in bottom button
         int bottomY = bottomConnection.frame.origin.y + bottomAdjustment;
         
         UIView *line = [[UIView alloc] initWithFrame:CGRectMake(topX, topY, 3, bottomY - topY)];
-        line.backgroundColor = [UIColor colorWithRed:(192/255.0) green:(192/255.0) blue:(192/255.0) alpha:1];
+        line.backgroundColor = [self GetGrayColor];
         [self.view addSubview:line];
         [self.view sendSubviewToBack:line];
+        
+        [verticalLines addObject:line];
     }
+    
+    self.verticalLines = verticalLines;
+}
+
+-(void)DrawHorizontalConnectingLines
+{
+    // We want to connect lines from the left color buttons to the rightmost column of color cells
+    NSArray *leftConnections = self.leftColorButtons;
+    int rowCount = (int)[self.colorCellSections count];
+
+    // Add rightmost cell of each row to rightConnections
+    NSMutableArray *rightConnections = [NSMutableArray array];
+    for (int i=0; i<rowCount; i++)
+    {
+        NSArray *row = [self.colorCellSections objectAtIndex:i];
+        int colCount = (int)[row count];
+        UIView *rightConnection = [row objectAtIndex:colCount - 1];
+        [rightConnections addObject:rightConnection];
+    }
+    
+    // We should assert that left connections and right connections have equal number of items
+    int itemCount = (int)[leftConnections count];
+    NSMutableArray *horizontalLines = [NSMutableArray array];
+    for (int i=0; i<itemCount; i++)
+    {
+        // Draw line
+        UIView *leftConnection = [leftConnections objectAtIndex:i];
+
+        int yAdjustment = -1; // Account for the fact that our width is 3 pixels
+        int leftY = leftConnection.frame.origin.y + leftConnection.frame.size.height / 2 + yAdjustment;
+        int leftAdjustment = -13;   // Accounts for extra spacing in left button
+        int leftX = leftConnection.frame.origin.x + leftConnection.frame.size.width + leftAdjustment;
+        
+        UIImageView *rightConnection = [rightConnections objectAtIndex:i];;
+        int rightAdjustment = 3;   // Accounts for extra spacing in right button
+        int rightX = rightConnection.frame.origin.x + rightAdjustment;
+        
+        UIView *line = [[UIView alloc] initWithFrame:CGRectMake(leftX, leftY, rightX - leftX, 3)];
+        line.backgroundColor = [self GetGrayColor];
+        [self.view addSubview:line];
+        [self.view sendSubviewToBack:line];
+        
+        [horizontalLines addObject:line];
+    }
+    
+    self.horizontalLines = horizontalLines;
 }
 
 -(void)UpdateColorCells
@@ -229,5 +289,24 @@
     }
 }
 
+-(UIColor *) GetGrayColor
+{
+    return [UIColor colorWithRed:(192/255.0) green:(192/255.0) blue:(192/255.0) alpha:1];
+}
+
+-(UIColor *) GetBlueColor
+{
+    return [UIColor colorWithRed:(0/255.0) green:(38/255.0) blue:(255/255.0) alpha:1];
+}
+
+-(UIColor *) GetYellowColor
+{
+    return [UIColor colorWithRed:(255/255.0) green:(216/255.0) blue:(0/255.0) alpha:1];
+}
+
+-(UIColor *) GetRedColor
+{
+    return [UIColor colorWithRed:(255/255.0) green:(0/255.0) blue:(0/255.0) alpha:1];
+}
 
 @end
