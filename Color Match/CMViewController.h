c177063bd71ce39e4b23feceef0a565bad218ad0
Linda Chen <linda167@Lindas-Mac-Pro.local>
2014-01-03 18:44:14 -0800

    Initial commit

--- Color Match/CMViewController.h
+++ Color Match/CMViewController.h
@@ -9,5 +9,27 @@
 #import <UIKit/UIKit.h>
 
 @interface CMViewController : UIViewController
+- (IBAction)ColorButtonPressed:(id)sender;
+- (IBAction)GridButtonPressed:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *blueButton;
+@property (weak, nonatomic) IBOutlet UIButton *yellowButton;
+@property (weak, nonatomic) IBOutlet UIButton *redButton;
+@property (weak, nonatomic) IBOutlet UIButton *whiteButton;
+@property (weak, nonatomic) IBOutlet UIButton *Top1Button;
+@property (weak, nonatomic) IBOutlet UIButton *Top2Button;
+@property (weak, nonatomic) IBOutlet UIButton *Top3Button;
+@property (weak, nonatomic) IBOutlet UIButton *Left1Button;
+@property (weak, nonatomic) IBOutlet UIButton *Left2Button;
+@property (weak, nonatomic) IBOutlet UIButton *Left3Button;
+@property (weak, nonatomic) IBOutlet UIImageView *Cell00;
+@property (weak, nonatomic) IBOutlet UIImageView *Cell01;
+@property (weak, nonatomic) IBOutlet UIImageView *Cell02;
+@property (weak, nonatomic) IBOutlet UIImageView *Cell10;
+@property (weak, nonatomic) IBOutlet UIImageView *Cell11;
+@property (weak, nonatomic) IBOutlet UIImageView *Cell12;
+@property (weak, nonatomic) IBOutlet UIImageView *Cell20;
+@property (weak, nonatomic) IBOutlet UIImageView *Cell21;
+@property (weak, nonatomic) IBOutlet UIImageView *Cell22;
 
 @end
--- Color Match/CMViewController.m
+++ Color Match/CMViewController.m
@@ -10,6 +10,12 @@
 
 @interface CMViewController ()
 
+@property NSInteger selectedColor;
+@property NSArray *topColorButtons;
+@property NSMutableArray *topColorsState;
+@property NSArray *leftColorButtons;
+@property NSMutableArray *leftColorsState;
+@property NSMutableArray *colorCellSections;
 @end
 
 @implementation CMViewController
@@ -18,6 +24,17 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.topColorButtons = [NSArray arrayWithObjects:_Top1Button,_Top2Button, _Top3Button, nil];
+    self.leftColorButtons = [NSArray arrayWithObjects:_Left1Button,_Left2Button, _Left3Button, nil];
+    
+    self.topColorsState = [NSMutableArray arrayWithObjects:[NSNumber numberWithInt:0], [NSNumber numberWithInt:0], [NSNumber numberWithInt:0], nil];
+    self.leftColorsState = [NSMutableArray arrayWithObjects:[NSNumber numberWithInt:0], [NSNumber numberWithInt:0], [NSNumber numberWithInt:0], nil];
+    
+    NSArray *row1 = [NSArray arrayWithObjects:_Cell00,_Cell01, _Cell02, nil];
+    NSArray *row2 = [NSArray arrayWithObjects:_Cell10,_Cell11, _Cell12, nil];
+    NSArray *row3 = [NSArray arrayWithObjects:_Cell20,_Cell21, _Cell22, nil];
+    
+    self.colorCellSections = [NSMutableArray arrayWithObjects:row1, row2, row3, nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +42,118 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)ColorButtonPressed:(id)sender {
+    NSArray* buttons = [NSArray arrayWithObjects:_whiteButton,_blueButton,_redButton, _yellowButton, nil];
+    
+    for (UIButton* button in buttons){
+        if (button == sender){
+            button.selected = YES;
+            
+            if (button == _whiteButton)
+                self.selectedColor = 0;
+            if (button == _blueButton)
+                self.selectedColor = 1;
+            else if (button == _redButton)
+                self.selectedColor = 2;
+            else if (button == _yellowButton)
+                self.selectedColor = 3;
+        }
+        else {
+            button.selected = NO;
+        }
+    }
+}
+
+- (IBAction)GridButtonPressed:(id)sender {
+    // Update filled color on the button being clicked
+    int selectedColor = self.selectedColor;
+    if (selectedColor == 0)
+    {
+        UIImage *btnImage1 = [UIImage imageNamed:@"EmptyCircle.png"];
+        [sender setImage:btnImage1 forState:UIControlStateNormal];
+    }
+    else if (selectedColor == 1)
+    {
+        UIImage *btnImage1 = [UIImage imageNamed:@"FilledCircleBlue.png"];
+        [sender setImage:btnImage1 forState:UIControlStateNormal];
+    }
+    else if (selectedColor == 2)
+    {
+        UIImage *btnImage1 = [UIImage imageNamed:@"FilledCircleRed.png"];
+        [sender setImage:btnImage1 forState:UIControlStateNormal];
+    }
+    else if (selectedColor == 3)
+    {
+        UIImage *btnImage1 = [UIImage imageNamed:@"FilledCircleYellow.png"];
+        [sender setImage:btnImage1 forState:UIControlStateNormal];
+    }
+    
+    // Update color state on top and left bar
+    NSNumber* wrappedSelectedColor = [NSNumber numberWithInt:selectedColor];
+    NSInteger topIndex = [self.topColorButtons indexOfObject:sender];
+    if (topIndex != NSNotFound)
+    {
+        [self.topColorsState replaceObjectAtIndex:topIndex withObject:wrappedSelectedColor];
+    }
+    else
+    {
+        NSInteger leftIndex = [self.leftColorButtons indexOfObject:sender];
+        // Should not be NSNotFound at ths point
+        [self.leftColorsState replaceObjectAtIndex:leftIndex withObject:wrappedSelectedColor];
+    }
+    
+    // Trigger update of color cells
+    [self UpdateColorCells];
+}
+
+-(void)UpdateColorCells
+{
+    int sectionsCount = [self.colorCellSections count];
+    for (int i=0; i<sectionsCount; i++)
+    {
+        NSArray *row = [self.colorCellSections objectAtIndex:i];
+        int rowLength = [row count];
+        for (int j=0; j<rowLength; j++)
+        {
+            int topColor = [(NSNumber *)[self.topColorsState objectAtIndex:j] intValue];
+            int leftColor = [(NSNumber *)[self.leftColorsState objectAtIndex:i] intValue];
+            
+            UIImage *image;
+            if (topColor == 0 && leftColor == 0)
+            {
+                image = [UIImage imageNamed:@"BlockWhite.png"];
+            }
+            else if ((topColor == 1 && leftColor == 1) || (topColor == 1 && leftColor == 0) || (topColor == 0 && leftColor == 1))
+            {
+                image = [UIImage imageNamed:@"BlockBlue.png"];
+            }
+            else if ((topColor == 2 && leftColor == 2) || (topColor == 2 && leftColor == 0) || (topColor == 0 && leftColor == 2))
+            {
+                image = [UIImage imageNamed:@"BlockRed.png"];
+            }
+            else if ((topColor == 3 && leftColor == 3) || (topColor == 3 && leftColor == 0) || (topColor == 0 && leftColor == 3))
+            {
+                image = [UIImage imageNamed:@"BlockYellow.png"];
+            }
+            else if ((topColor == 1 && leftColor == 2) || (topColor == 2 && leftColor == 1))
+            {
+                image = [UIImage imageNamed:@"BlockPurple.png"];
+            }
+            else if ((topColor == 1 && leftColor == 3) || (topColor == 3 && leftColor == 1))
+            {
+                image = [UIImage imageNamed:@"BlockGreen.png"];
+            }
+            else if ((topColor == 2 && leftColor == 3) || (topColor == 3 && leftColor == 2))
+            {
+                image = [UIImage imageNamed:@"BlockOrange.png"];
+            }
+            
+            UIImageView *imageView = [row objectAtIndex:j];
+            [imageView setImage:image];
+        }
+    }
+}
+
 
 @end
